e6967899d8bde9918587add48ba0e537723f134c
dotranminhchu <dotranminhchu@gmail.com>
2023-05-19 16:43:19 +0700

    [FIX]

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
@@ -6,28 +6,26 @@ import { roomHandler } from "./room";
 
 const app = express();
 app.use(cors);
-const port = 8089
+const port = 8089;
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
-    roomHandler(socket);
-    socket.on("disconnect", () => {
-        console.log("user disconnected");
-    });
+  console.log("a user connected");
+  roomHandler(socket);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
-server.get("/", (req: Request, res: Response) => {
+app.get("/", (_req: Request, res: Response) => {
   res.send("<h1>Nhiệt liệt chào mừng quý vị đại coder!</h1>");
 });
-
-
 server.listen(port, () => {
-    console.log(`listening on *:${port}`);
+  console.log(`listening on *:${port}`);
 });
